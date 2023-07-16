4e9bc4b60553ae065828ba94a95c13c622d21f6a
Bence Márkus <markus.bence@icloud.com>
2023-07-16 16:13:39 +0200

    dep and config change

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,22 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
 module.exports = {
 	root: true,
-	env: {
-		es6: true,
-		node: true,
-	},
-	plugins: ['@typescript-eslint'],
-	parser: '@typescript-eslint/parser',
+	extends: [
+		'eslint:recommended',
+		'plugin:vue/vue3-essential',
+		'@vue/eslint-config-typescript',
+		'@vue/eslint-config-prettier/skip-formatting',
+	],
 	parserOptions: {
 		ecmaVersion: 'latest',
-		sourceType: 'module',
 	},
-	extends: ['prettier', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-	ignorePatterns: ['node_modules/', 'dist/', 'coverage/', 'src/**/*.test.ts'],
 	rules: {
-		'no-tabs': 0,
-		'no-unused-expressions': 1,
+		'no-var': 'error',
+		'prefer-const': 'error',
+		'no-unused-vars': 'error',
+		'no-unused-expressions': 'error',
 		indent: [1, 'tab', { SwitchCase: 1 }],
 		'@typescript-eslint/no-unused-vars': 1,
 		'@typescript-eslint/consistent-type-imports': 'error',
@@ -22,4 +24,5 @@ module.exports = {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 	},
+	ignorePatterns: ['node_modules/', 'dist/', 'coverage/', 'src/**/*.spec.ts'],
 };
